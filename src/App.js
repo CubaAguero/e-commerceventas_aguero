bd9d1799fe5d795404d60d0092616c7862a6b90d
CubaAguero <cuba56_9@hotmail.com>
2021-06-04 19:25:19 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          E-commerce Ventas Aguero
+          E-commerce Ventas Agüero
         </p>
         <a
           className="App-link"
